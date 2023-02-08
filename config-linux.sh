34b6c124704dd9241ff1ee81fbebe1750caf3467
Pablo Alves <51347793+pabloalvesdev@users.noreply.github.com>
2023-02-08 13:59:33 -0300

    Update config-linux.sh

--- config-linux.sh
+++ config-linux.sh
@@ -103,3 +103,9 @@ sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] h
 
 # Install for both desktop and web modes:
 sudo apt install pgadmin4
+
+
+# Install spotify
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
